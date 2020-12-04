202e6e0b4097e7f9625cec2e0fb0eeee6a70e28d
Sara <sbehnke33@yahoo.com>
2020-12-03 19:35:56 -0600

    add

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,11 @@
+//start psuedo code
+
+// create map using leaflet that plots all earthquakes from the data set based on their long & lat
+// data markers should reflect the magnitude of the earthquke by their size&depth of the earthqake by color
+// earthquakes w/ higher magnitudes should appear larger and ones with greater depth should appear darker in color
+
+// HINT!!!! the depth of the earth can be found as the 3rd coordinate
+
+// popups that give add info about the eq when marker is clicked
+
+// legend that gives context for map data
