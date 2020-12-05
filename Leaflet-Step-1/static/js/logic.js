de03b0068ed2d7f36dda717fd459d41aed7739b8
Sara <sbehnke33@yahoo.com>
2020-12-05 14:21:55 -0600

    Update logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -10,16 +10,29 @@
 
 // legend that gives context for map data
 
-const link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-d3.json(link, function(earthquakes) {
-    console.log(earthquakes);
-    createMap(earthquakes)
     
 var myMap = L.map("map-id",{
     center: [37.09, -95.71],
     zoom: 5
 });
+
+
+
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "light-v10",
+    accessToken: API_KEY
+}).addTo(myMap)
+
+const link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+d3.json(link, function(earthquakes) {
+    console.log(earthquakes);
+    createMap(earthquakes)});
 
 function createMap(earthquakesData) {
     L.geoJSON(earthquakesData, {
@@ -27,18 +40,13 @@ function createMap(earthquakesData) {
     })
 }
 
-var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "light-v10",
-    accessToken: API_KEY
-});
-
-
-
-
-lightmap.addTo(myMap)
-
-})
+function createCircleMarker(feature, latlng ){
+      var markerOptions = {
+        radius: markerSize(feature.properties.mag),
+        fillColor: getColor(feature.properties.mag),
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+      }
+      return L.circleMarker( latlng, markerOptions );
+    };
